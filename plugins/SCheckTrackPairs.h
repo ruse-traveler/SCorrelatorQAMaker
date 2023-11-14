8ba2f5e0aaf1cc918d42983a12e6fe06c60b1ecf
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-11-14 17:39:51 -0500

    Create track pair check plugin

---
+++ plugins/SCheckTrackPairs.h
@@ -0,0 +1,80 @@
+// ----------------------------------------------------------------------------
+// 'SCheckTrackPairs.h'
+// Derek Anderson
+// 11.14.2023
+//
+// SCorrelatorQAMaker plugin to iterate through
+// all pairs of tracks in an event and fill
+// tuples/histograms comparing them.
+// ----------------------------------------------------------------------------
+
+#ifndef SCHECKTRACKPAIRS_H
+#define SCHECKTRACKPAIRS_H
+
+// c++ utilities
+#include <string>
+// f4a libraries
+#include <fun4all/SubsysReco.h>
+#include <fun4all/Fun4AllHistoManager.h>
+// phool libraries
+#include <phool/phool.h>
+#include <phool/getClass.h>
+#include <phool/PHIODataNode.h>
+#include <phool/PHNodeIterator.h>
+#include <phool/PHCompositeNode.h>
+// tracking libraries
+#include <trackbase_historic/SvtxTrack.h>
+#include <trackbase_historic/SvtxTrackMap.h>
+#include <trackbase_historic/TrackAnalysisUtils.h>
+// root utilities
+#include <TH1.h>
+#include <TH2.h>
+#include <TF1.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TMath.h>
+#include <TNtuple.h>
+// analysis utilities
+#include "SBaseQAPlugin.h"
+
+// make common namespaces implicit
+using namespace std;
+using namespace findNode;
+
+
+
+namespace SColdQcdCorrelatorAnalysis {
+
+  // SCheckTrackPairsConfig definition ----------------------------------------
+
+  struct SCheckTrackPairsConfig {
+
+    float ptMin = 0.;
+
+  };  // end SCheckTrackPairsConfig
+
+
+
+  // SCheckTrackPairs definition ----------------------------------------------
+
+  class SCheckTrackPairs : public SubsysReco, SBaseQAPlugin<SCheckTrackPairsConfig> {
+
+    public:
+
+      // F4A methods
+      int Init(PHCompositeNode*)          override;
+      int process_event(PHCompositeNode*) override;
+      int End(PHCompositeNode*)           override;
+
+    private:
+
+      // TODO put in implementation
+      void InitTuples();
+
+  };  // end SCheckTrackPairs
+
+}  // end SColdQcdCorrelatorAnalysis namespace
+
+#endif
+
+// end ------------------------------------------------------------------------
